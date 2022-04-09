381f490ddb9bba8e6eea0573029de0ded56e7a77
German Ivanov <germanivanov0719@gmail.com>
2022-04-10 02:25:18 +0300

    Fix minor errors in test

--- test_db.py
+++ test_db.py
@@ -12,6 +12,8 @@ from data.events import Event
 def clear_db():
     try:
         os.remove("db/db.sqlite")
+    except FileNotFoundError:
+        pass
     except Exception:
         raise AssertionError("Unable to remove database")
 
@@ -20,22 +22,21 @@ def test_db():
     clear_db()
     db_session.global_init("db/db.sqlite")
     db_sess = db_session.create_session()
-    assert db_sess
+    assert db_sess != 0
     # User
     user = User(
-        username="testuser",
         name="User1",
         about="I'm a user1",
         email="testuser@users.com",
     )
     user.set_password("123")
-    assert user
+    assert user != 0
     db_sess.add(user)
     # Place
     place = Place(
         name="Place1", address="On Earth", about="It is a great place"
     )
-    assert place
+    assert place != 0
     db_sess.add(place)
     # Place controlled
     user = db_sess.query(User).first()
@@ -54,7 +55,7 @@ def test_db():
     db_sess.add(order)
     # Commit
     db_sess.commit()
-    assert db_sess
+    assert db_sess != 0
 
 
 if __name__ == "__main__":
