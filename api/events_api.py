fba0fef37144e54c94acb4cf3c436358e919beeb
logeval <loginova032@gmail.com>
2022-04-26 00:33:37 +0300

    create api for places and events

---
+++ api/events_api.py
@@ -0,0 +1,60 @@
+import flask
+from flask import request, jsonify
+from data import db_session
+from data.events import Event
+
+blueprint = flask.Blueprint("events_api", __name__, template_folder="templates")
+
+
+@blueprint.route("/api/events")
+def get_events():
+    db_sess = db_session.create_session()
+    events = db_sess.query(Event).all()
+    return jsonify(
+        {
+            "events": [
+                item.to_dict()
+                for item in events
+            ]
+        }
+    )
+
+
+@blueprint.route("/api/jobs/<int:events_id>", methods=["GET"])
+def get_one_place(jobs_id):
+    db_sess = db_session.create_session()
+    events = db_sess.query(Event).get(jobs_id)
+    if not events:
+        return jsonify({"error": "Not found"})
+    return jsonify(
+        {
+            "places": events.to_dict()
+        }
+    )
+
+
+@blueprint.route("/api/places", methods=["POST"])
+def add_event():
+    if not request.json:
+        return jsonify({"error": "Empty request"})
+    elif not all(
+            key in request.json
+            for key in ["name", "description", "created_datetime", "place_id", "required_age", "notes"]
+    ):
+        return jsonify({"error": "Bad request"})
+    db_sess = db_session.create_session()
+    event = Event(
+        id=request.json["id"],
+        name=request.json["name"],
+        address=request.json["description"],
+        about=request.json["created_datetime"],
+        created_datetime=request.json["place_id"],
+        place_id=request.json["place_id"],
+        required_age=request.json["required_age"],
+        notes=request.json["notes"]
+    )
+    if db_sess.query(Event).filter(Event.id == event.id).first():
+        return jsonify({"error": "Id already exists"})
+    db_sess.add(event)
+    db_sess.commit()
+    return jsonify({"success": "OK"})
---
+++ api/places_api.py
@@ -0,0 +1,73 @@
+import flask
+from flask import request, jsonify
+from data import db_session
+from data.places import Place
+
+blueprint = flask.Blueprint("places_api", __name__, template_folder="templates")
+
+
+@blueprint.route("/api/places")
+def get_places():
+    db_sess = db_session.create_session()
+    places = db_sess.query(Place).all()
+    return jsonify(
+        {
+            "places": [
+                item.to_dict(
+                    only=(
+                        "id",
+                        "name",
+                        "address",
+                        "about",
+                        "created_datetime"
+                    )
+                )
+                for item in places
+            ]
+        }
+    )
+
+
+@blueprint.route("/api/jobs/<int:place_id>", methods=["GET"])
+def get_one_place(jobs_id):
+    db_sess = db_session.create_session()
+    places = db_sess.query(Place).get(jobs_id)
+    if not places:
+        return jsonify({"error": "Not found"})
+    return jsonify(
+        {
+            "places": places.to_dict(
+                only=(
+                    "id",
+                    "name",
+                    "address",
+                    "about",
+                    "created_datetime"
+                )
+            )
+        }
+    )
+
+
+@blueprint.route("/api/places", methods=["POST"])
+def add_place():
+    if not request.json:
+        return jsonify({"error": "Empty request"})
+    elif not all(
+            key in request.json
+            for key in ["name", "address", "about", "created_datetime"]
+    ):
+        return jsonify({"error": "Bad request"})
+    db_sess = db_session.create_session()
+    place = Place(
+        id=request.json["id"],
+        name=request.json["name"],
+        address=request.json["address"],
+        about=request.json["about"],
+        created_datetime=request.json["created_datetime"]
+    )
+    if db_sess.query(Place).filter(Place.id == place.id).first():
+        return jsonify({"error": "Id already exists"})
+    db_sess.add(place)
+    db_sess.commit()
+    return jsonify({"success": "OK"})
